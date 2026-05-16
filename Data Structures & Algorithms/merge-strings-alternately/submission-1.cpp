4da40f615641e2304b6486fba331c61a161b6666
Evgenii Kondratenko <163011142+k-evgenii@users.noreply.github.com>
2026-05-16 02:06:50 +0100

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        string ans;
+        ans.reserve(n1 + n2);
+        int m = min(n1, n2);
+
+        for(int i{0}; i < m; i++){
+            ans.push_back(word1[i]);
+            ans.push_back(word2[i]);
+        }
+        if(n1 > n2){
+            ans.append(word1, m, n1 - m);
+            }
+        if(n1 < n2){
+            ans.append(word2, m, n2 - m); 
+        }
+        return ans;
+    }
+};
